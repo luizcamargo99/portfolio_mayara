248c663ada5f5e53cfecd99130e103205f190dbf
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-24 22:01:18 -0300

    implementações

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,13 @@
+function init() {
+    preencherFooter();   
+}
+
+function preencherFooter() {
+    const footerText = document.getElementById('texto-footer');
+    const date = new Date();
+    footerText.innerHTML = `Todos os Direitos Reservados © ${date.getFullYear()} |<b> OAB/SP 443.659 </b>| Site criado por <a href="https://luizcamargo.dev" target="blank">Luiz Camargo</a>`;
+}
+
 function toggleMenu() {
     tratarMenu();
     tratarLinhasMenu();
