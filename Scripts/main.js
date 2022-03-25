ffae070f218f9b1db684e71ece75653c5639a3ed
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-24 23:08:50 -0300

    ADD DIREITO CIVIL

--- Scripts/main.js
+++ Scripts/main.js
@@ -8,25 +8,3 @@ function preencherFooter() {
     footerText.innerHTML = `Todos os Direitos Reservados © ${date.getFullYear()} |<b> OAB/SP 443.659 </b>| Site criado por <a href="https://luizcamargo.dev" target="blank">Luiz Camargo</a>`;
 }
 
-function toggleMenu() {
-    tratarMenu();
-    tratarLinhasMenu();
-}
-
-function tratarMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-}
-
-function tratarLinhasMenu() {
-    const linhas = document.getElementsByClassName('linha');
-    for (let index = 0; index < linhas.length; index++) {
-        linhas[index].classList.toggle('active');
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    const btnMobile = document.getElementById('btn-mobile');
-    btnMobile.addEventListener('click', toggleMenu);  
-});
-
