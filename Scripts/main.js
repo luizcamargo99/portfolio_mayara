44d6c34ad90f2b8765cda3de059f32af1855a0c1
Luiz Camargo <luiz.zito@hotmail.com>
2022-04-17 13:21:02 -0300

    fix

--- Scripts/main.js
+++ Scripts/main.js
@@ -7,7 +7,3 @@ function preencherFooter() {
     const date = new Date();
     footerText.innerHTML = `Todos os Direitos Reservados © ${date.getFullYear()} |<b> OAB/SP 443.659 </b>| Site criado por <a href="https://luizcamargo.dev" target="blank">Luiz Camargo</a>`;
 }
-
-function virarCard (card) {
-    card.classList.toggle('active-card');
-}
